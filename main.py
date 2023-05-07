ba335754ddac5a58571ba3dd0acfbbf472bee418
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-05-07 05:01:09 -0400

    Initial Commit

--- main.py
+++ main.py
@@ -177,8 +177,14 @@ def dice_coef(y_true, y_pred, smooth=1):
     union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3])
     return K.mean( (2. * intersection + smooth) / (union + smooth), axis=0)
 def dice_p_bce(in_gt, in_pred):
+    print("the shape of in_gt is",in_gt.shape)
+    print("the shape of in_pred is",in_pred.shape)
+    
     return 1e-3*binary_crossentropy(in_gt, in_pred) - dice_coef(in_gt, in_pred)
 def true_positive_rate(y_true, y_pred):
+    print("the shape of y_true in true_positive_rate is",y_true.shape)
+    print("the shape of y_pred in true_positive_rate is",y_pred.shape)
+    
     return K.sum(K.flatten(y_true)*K.flatten(K.round(y_pred)))/K.sum(y_true)
 seg_model.compile(optimizer=Adam(1e-4, decay=1e-6), loss=dice_p_bce, metrics=[dice_coef, 'binary_accuracy', true_positive_rate])
 
