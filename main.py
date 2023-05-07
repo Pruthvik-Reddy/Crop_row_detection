aee324200d3276872095cd4130b7181f83db5d5b
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-05-07 04:15:37 -0400

    Initial Commit

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import gc; gc.enable() # memory is tight
 from skimage.morphology import label
 from utils import *
 import random
+from keras.preprocessing.image import ImageDataGenerator
+
 
 
 montage_rgb = lambda x: np.stack([montage(x[:, :, :, i]) for i in range(x.shape[3])], -1)
@@ -48,4 +50,47 @@ print('y', train_y.shape, train_y.min(), train_y.max())
 #MAKE VALIDATION SET
 valid_x, valid_y = next(make_image_gen(TEST_IMGS,len(TEST_IMGS)))
 print(valid_x.shape, valid_y.shape)
+
+
+#Augment Data
+dg_args = dict(featurewise_center = False, 
+                  samplewise_center = False,
+                  rotation_range = 15, 
+                  width_shift_range = 0.1, 
+                  height_shift_range = 0.1, 
+                  shear_range = 0.01,
+                  zoom_range = [0.9, 1.25],  
+                  horizontal_flip = True, 
+                  vertical_flip = False,
+                  fill_mode = 'reflect',
+                   data_format = 'channels_last')
+# brightness can be problematic since it seems to change the labels differently from the images 
+if AUGMENT_BRIGHTNESS:
+    dg_args[' brightness_range'] = [0.5, 1.5]
+image_gen = ImageDataGenerator(**dg_args)
+
+if AUGMENT_BRIGHTNESS:
+    dg_args.pop('brightness_range')
+label_gen = ImageDataGenerator(**dg_args)
+
+def create_aug_gen(in_gen, seed = None):
+    np.random.seed(seed if seed is not None else np.random.choice(range(9999)))
+    for in_x, in_y in in_gen:
+        seed = np.random.choice(range(9999))
+        # keep the seeds syncronized otherwise the augmentation to the images is different from the masks
+        g_x = image_gen.flow(255*in_x, 
+                             batch_size = in_x.shape[0], 
+                             seed = seed, 
+                             shuffle=True)
+        g_y = label_gen.flow(in_y, 
+                             batch_size = in_x.shape[0], 
+                             seed = seed, 
+                             shuffle=True)
+
+        yield next(g_x)/255.0, next(g_y)
+
+cur_gen = create_aug_gen(train_gen)
+t_x, t_y = next(cur_gen)
+print('x', t_x.shape, t_x.dtype, t_x.min(), t_x.max())
+print('y', t_y.shape, t_y.dtype, t_y.min(), t_y.max())
 
